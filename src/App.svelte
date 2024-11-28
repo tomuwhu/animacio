98cc2673f01a8e5a024ae68f8df398f38805f12f
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-11-28 12:25:35 +0100

    pattog

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,21 @@
 <script>
   var x = 0;
   var y = 0;
+  var xi = 1;
+  var yi = 1;
+  setInterval(() => {
+    x += xi
+    y += yi
+    if (x >= window.innerWidth || x<=0) {
+      xi *= -1
+    }
+    if (y >= window.innerHeight-35 || y<=0) {
+      yi *= -1
+    }
+  })
 </script>
 
 <main>
-  <input type="range" bind:value={x}>
-  <input type="range" bind:value={y}>
   <div class="A" style="left: {x}px; top: {y}px;">O</div>
 </main>
 
