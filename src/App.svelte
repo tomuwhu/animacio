684b437d9dd1ccae95e4568a7724dd19a7acd489
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-11-28 12:44:11 +0100

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,23 @@
 <script>
-  var x = 0;
-  var y = 0;
-  var xi = 1;
-  var yi = 1;
+  var [x, y, xi, yi] = [1, 1, 1, 0];
   setInterval(() => {
     x += xi
     y += yi
-    if (x >= window.innerWidth || x<=0) {
+    yi += 0.01
+    xi *= 0.9999 
+    yi *= 0.9999
+    if (x >= window.innerWidth-60 || x<=0) 
       xi *= -1
-    }
-    if (y >= window.innerHeight-35 || y<=0) {
+    if (y >= window.innerHeight-35 || y<=0)
       yi *= -1
-    }
   })
 </script>
-
 <main>
-  <div class="A" style="left: {x}px; top: {y}px;">O</div>
+  <div class="A" style="left: {x}px; top: {y}px;">DVD</div>
 </main>
-
 <style>
-
   div.A {
     position: fixed;
     font-size: 30px;
   }
-
 </style>
