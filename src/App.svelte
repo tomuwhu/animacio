c41c23152b9b2de2cd45df4814ea70e97f619dea
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-11-28 13:04:50 +0100

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,41 @@
 <script>
-  var [x, y, xi, yi] = [1, 1, 1, 0];
+  var [x, y, xi, yi] = [1, 1, 0, 0];
+  var ex = 0, ey = 0
   setInterval(() => {
     x += xi
     y += yi
-    yi += 0.01
-    xi *= 0.9999 
-    yi *= 0.9999
-    if (x >= window.innerWidth-60 || x<=0) 
-      xi *= -1
-    if (y >= window.innerHeight-35 || y<=0)
-      yi *= -1
+    xi -= (x - ex) * 0.0001
+    yi -= (y - ey) * 0.0001
+    if (x >= window.innerWidth-60)
+      xi = -Math.abs(xi) / 2
+    if (x <= 0) 
+      xi = Math.abs(xi) / 2
+    if (y >= window.innerHeight-35)
+      yi = -Math.abs(yi) / 2
+    if (y <= 0)
+    yi = Math.abs(yi) / 2
   })
 </script>
 <main>
-  <div class="A" style="left: {x}px; top: {y}px;">DVD</div>
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div class="eg" on:mousemove={(e) => {
+    ex = e.clientX; ey = e.clientY
+  }}>
+    <div class="A" style="left: {x}px; top: {y}px;">DVD</div>
+    <div class="B" style="left: {ex}px; top: {ey}px;">O</div>
+  </div>
 </main>
 <style>
-  div.A {
+  div.eg {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    cursor: none;
+    width: 100%;
+    height: 100%;
+  }
+
+  div.A, div.B {
     position: fixed;
     font-size: 30px;
   }
