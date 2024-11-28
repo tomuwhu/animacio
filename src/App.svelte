71b65df65c881754d1bf1a9e97a9100958bbe3fa
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-11-28 12:19:14 +0100

    binding

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  var x = 0;
+  var y = 0;
+</script>
+
+<main>
+  <input type="range" bind:value={x}>
+  <input type="range" bind:value={y}>
+  <div class="A" style="left: {x}px; top: {y}px;">O</div>
+</main>
+
+<style>
+
+  div.A {
+    position: fixed;
+    font-size: 30px;
+  }
+
+</style>
